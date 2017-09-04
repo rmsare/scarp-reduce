2c748a8b97ade5c24c55721fd4769b2643ad1b50
rmsare <robertmsare@gmail.com>
2017-09-03 18:08:42 -0700

    Fix typos in compare, reduce and add set methods

--- Worker.py
+++ Worker.py
@@ -1,19 +1,22 @@
 """
 Worker classes for template matching
 """
+
+import os
 import dem, scarplet
+import numpy as np
 
 import uuid
 
-
+from WindowedTemplate import Scarp
 
 
 NUM_ANGLES = 181
-NUM_AGES = 32
+NUM_AGES = 35
 MAX_AGE = 3.5
 
-PAD_DX = 200
-PAD_DY = 200
+PAD_DX = 250
+PAD_DY = 250
 
 
 class Worker(object):
@@ -22,29 +25,41 @@ class Worker(object):
         pass
 
 
-class Matcher(Worker):
+class Matcher(object):
     
     def __init__(self, source):
         self.age = None
+        self. d = None
         self.path = '/efs/results/'
-        self.filename = None 
-        self.source = None
-        self.dx = None
-        self.dy = None
-        
+        self.set_source(source)
+    
     def clip_results(pad_dx, pad_dy):
         i = pad_dx / self.dx
         j = pad_dy / self.dy
         self.results = self.results[:, j:-j, i:-i]
 
+    def load_data(self):
+        self.data = dem.DEMGrid(self.source)
+        self.data._pad_boundary(PAD_DX, PAD_DY)
+    
+    def match_template(data, d, age):
+        return scarplet.calculate_best_fit_parameters(data, Scarp, d, age)
+
+    def process(self, ages):
+        self.load_data()
+        d = 100
+        for age in ages:
+            self.set_params(age, d)
+            self.save_template_match()
+
     def save_template_match(self):
-        self.results = match_template(self.data, self.age)
-        self.clip_results(PAD_DX, PAD_DY)
+        self.results = self.match_template(self.data, self.d, self.age)
+        self.clip_results(PAD_DX, PAD_DY) # Assume data is padded!
         np.save(self.path + self.filename, self.results)
 
-    def set_params(self, age, angle):
+    def set_params(self, age, d):
         self.age = age
-        self.angle = angle
+        self.d = d
         self.filename = 'results_' + str(self.age) + '.npy'
 
     def set_source(self, source):
@@ -54,17 +69,17 @@ class Matcher(Worker):
         self.dy = data._georef_info.dy
 
       
-class Reducer(Worker):
+class Reducer(object):
 
     def __init__(self, path):
         self.path = path
         self.best_results = None
         
-    def compare(self, file1, file2, level):
+    def compare(self, file1, file2):
         data1 = np.load(self.path + file1)
         data2 = np.load(self.path + file2)
         mask = data1[-1,:,:] > data2[-1,:,:]
-        data2[mask,:] = data1[mask,:]
+        data2[:,mask] = data1[:,mask]
         
         os.remove(self.path + file1)
         os.remove(self.path + file2)
@@ -81,4 +96,8 @@ class Reducer(Worker):
             self.compare(results1, results2)
             results = os.listdir(self.path)
         self.best_results = os.listdir(self.path)[0]
+
+    def set_path(self, path):
+        self.path = path
                 
+
