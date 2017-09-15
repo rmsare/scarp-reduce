7d21737ff32192f803cde3935862acb11e6b1c24
Ubuntu <ubuntu@ip-172-31-5-68.us-west-2.compute.internal>
2017-09-15 01:21:23 +0000

    Misc type enforcement, testing

--- match.py
+++ match.py
@@ -16,11 +16,12 @@ if __name__ == "__main__":
     if min_age == max_age:
         ages = [min_age]
     else:
-        num_ages = (max_age - min_age) / d_age
+        num_ages = int((max_age - min_age) / d_age)
         ages = np.linspace(min_age, max_age, num_ages)
 
     local_data_directory = '/efs/data/'
-    tiles = os.listdir(local_data_directory)
+    #tiles = os.listdir(local_data_directory)
+    tiles = ['test2000.tif']
 
     for tile in tiles:
         worker = Matcher(local_data_directory + tile)
