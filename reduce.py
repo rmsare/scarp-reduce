00646b0f1132574b9d6a50a814a6c81353a52c2b
Ubuntu <ubuntu@ip-172-31-5-68.us-west-2.compute.internal>
2017-09-15 22:34:03 +0000

    Save best results to bucket, take grid name as argument

--- reduce.py
+++ reduce.py
@@ -2,22 +2,31 @@ import os, sys
 sys.path.append('/home/ubuntu/scarplet-python/scarplet')
 import dem, scarplet
 
+from s3utils import save_file_to_s3
+
 from timeit import default_timer as timer
 from Worker import Matcher, Reducer
 
 
 if __name__ == "__main__":
     tile_name = sys.argv[1]
+    if tile_name[-1] is not '/':
+        tile_name = tile_name +'/'
+
+    num_files = int(sys.argv[2])
+
     # Launch Reducer instance
+    start = timer()
     reducer = Reducer('/efs/results/' + tile_name)
-    reducer.set_num_workers(num_workers)
+    reducer.set_num_files(num_files)
 
     # Reduce results as they arrive
-    start = timer()
     reducer.reduce()
     stop = timer()
-    print('Reduce:\t\t {} s'.format(stop-start))
+    print('Reduce:\t\t {:.2f} s'.format(stop-start))
 
     # Convert best results to TIFF
 
     # Save best results to S3
+    filename = os.listdir('/efs/results/' + tile_name)[0]
+    save_file_to_s3('/efs/results/' + tile_name + '/' + filename, tile_name + '/best_results.npy', bucket_name='scarp-testing')
