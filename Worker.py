48e23a872018f69720657e8c95c1a1d5f0265742
Ubuntu <ubuntu@ip-172-31-5-68.us-west-2.compute.internal>
2017-09-16 18:38:14 +0000

    Make results dir if it doesn't exist, misc placeholders

--- Worker.py
+++ Worker.py
@@ -33,23 +33,27 @@ class Matcher(object):
     def __init__(self, source):
         self.age = None
         self. d = None
+
         name = source.split('/')[-1][:-4]
         self.path = '/efs/results/' + name + '/'
+        if not os.path.exists(self.path):
+            os.mkdir(self.path
+
         self.source = source    
 
-    def clip_results(self, pad_dx, pad_dy):
+    def get_clip_boundaries(self, pad_dx, pad_dy):
         i = int(pad_dx / self.dx)
         j = int(pad_dy / self.dy)
-        self.results = self.results[:, j:-j, i:-i]
+        return i, j
 
     def load_data(self):
         self.data = dem.DEMGrid(self.source)
         self.dx = self.data._georef_info.dx
         self.dy = self.data._georef_info.dy
-        self.data._pad_boundary(PAD_DX, PAD_DY) # TODO: Pad data once and save
+        #self.data._fill_nodata() # TODO: Fill nodata once
+        #self.data._pad_boundary(PAD_DX, PAD_DY) # TODO: Pad data once and save
     
     def match_template(self):
-        #return scarplet.calculate_fit(self.data, Scarp, self.d, self.age, self.angle)
         return scarplet.calculate_best_fit_parameters(self.data, Scarp, self.d, self.age)
 
     def process(self, d, ages):
@@ -66,14 +70,15 @@ class Matcher(object):
 
     def save_template_match(self):
         self.results = self.match_template()
-        self.clip_results(PAD_DX, PAD_DY) # Assume data is padded!
+        #i, j = self.get_clip_boundaries(PAD_DX, PAD_DY) # Assume data is padded!
+        #np.save(self.path + self.filename, self.results[:, j:-j, i:-i])
         np.save(self.path + self.filename, self.results)
+        del self.results
 
     def set_params(self, age, d):
         self.age = age
         self.d = d
         self.filename = 'results_{:.2f}.npy'.format(self.age)
-        #self.filename = 'results_{:.2f}_{:.2f}.npy'.format(self.angle, self.age)
 
     def set_source(self, source):
         self.source = source
