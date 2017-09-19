ed0542159b9b6448d4221f072b7f8898f6a79357
rmsare <robertmsare@gmail.com>
2017-09-19 15:52:54 -0700

    Commenting and remove unused methods

--- Worker.py
+++ Worker.py
@@ -43,12 +43,9 @@ class Matcher(object):
 
         self.source = source    
 
-    def get_clip_boundaries(self, pad_dx, pad_dy):
-        i = int(pad_dx / self.dx)
-        j = int(pad_dy / self.dy)
-        return i, j
-
     def load_data(self):
+        # Load data from source file
+
         # XXX: Assume data has been interpolated to remove nans
         self.data = dem.DEMGrid(self.source)
         self.dx = self.data._georef_info.dx
@@ -56,34 +53,39 @@ class Matcher(object):
         self.data._pad_boundary(PAD_DX, PAD_DY) # TODO: Pad data once and save
     
     def match_template(self):
+        # Match template to data
+
         return scarplet.calculate_best_fit_parameters(self.data, Scarp, self.d, self.age)
 
     def process(self, d, ages):
+        # Match templates for a list of parameters
+
         print("Processing {}...".format(self.source))
         self.load_data()
         self.d = d
 
         for age in ages:
-            #start = timer()
             self.set_params(age, d)
             self.save_template_match()
-            #stop = timer()
-            #print("Fit template for {}, params d={:.0f}, kt={:.2f}".format(self.source, self.d, age))
-            #print("Elapsed time:\t {:.2f} s".format(stop-start))
 
     def save_template_match(self):
+        # Match template, and save clipped (valid) results
+
         self.results = self.match_template()
         # XXX: Assume data is padded!
         np.save(self.path + self.filename, self.results[:, PAD_DY:-PAD_DY, PAD_DX:-PAD_DX])
-        #np.save(self.path + self.filename, self.results)
         del self.results
 
     def set_params(self, age, d):
+        # Set worker template parameters
+
         self.age = age
         self.d = d
         self.filename = 'results_{:.2f}.npy'.format(self.age)
 
     def set_source(self, source):
+        # Set data source location
+
         self.source = source
 
       
@@ -97,6 +99,8 @@ class Reducer(object):
         self.best_results = None
         
     def compare(self, file1, file2):
+        # Compare two search step results ndarrays
+
         data1 = np.load(file1)
         data2 = np.load(file2)
         mask = data1[-1,:,:] > data2[-1,:,:]
@@ -108,13 +112,17 @@ class Reducer(object):
         return data2
     
     def mask_results(self, tile_name, results):
+        # Mask out nodata in results ndarray using saved mask
+
         mask = np.load('/efs/masks/' + tile_name + '_mask.npy')
         results[:, mask] = np.nan
         return results
 
-    def reduce(self, folder):
+    def reduce(self, directory):
+        # Reduce a single directory of results
+
         curdir = os.getcwd()
-        self.path = self.path + '/' + folder
+        self.path = self.path + '/' + directory
         results = os.listdir(self.path)
         os.chdir(self.path)
         
@@ -134,6 +142,8 @@ class Reducer(object):
         self.best_results = self.path + os.listdir(self.path)[0]
 
     def reduce_all_results(self):
+        # Reduce results in all directories until all search steps have been compared
+
         curdir = os.getcwd()
         subgrids = os.listdir(self.path)
         os.chdir(self.path)
@@ -158,9 +168,11 @@ class Reducer(object):
         print("processed {} files".format(files_processed))
         os.chdir(curdir)
                 
-
     def save_results(self):
+        # Save all reduced results to S3 bucket
+
         subgrids = os.listdir(self.path)
+        # XXX: Assumes each directory contains a single file containing fully reduced data!
         for tile in subgrids:
             best_file = self.path + '/' + tile + '/' + os.listdir(self.path + '/' + tile)[0]
             results = np.load(best_file)    
@@ -168,10 +180,9 @@ class Reducer(object):
             np.save(best_file, results)    
             save_file_to_s3(best_file, tile + '_results.npy', bucket_name='scarp-testing')
 
-    def save_best_results(self):
-        save_file_to_s3(self.best_results, self.tile_name + '_results.npy', bucket_name='scarp-testing')
-
     def set_num_files(self, num_files):
+        # Number of files per directory (= number of workers/search steps)
+
         self.num_files = num_files
     
     def set_path(self, path):
