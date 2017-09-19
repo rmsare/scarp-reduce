fb62e4d9a4f069a3905fb8692fdea6300ecaae63
rmsare <robertmsare@gmail.com>
2017-09-19 12:45:35 -0700

    Fix relative path bugs...

--- Worker.py
+++ Worker.py
@@ -49,10 +49,11 @@ class Matcher(object):
         return i, j
 
     def load_data(self):
+        # XXX: Assume data has been interpolated to remove nans
         self.data = dem.DEMGrid(self.source)
         self.dx = self.data._georef_info.dx
         self.dy = self.data._georef_info.dy
-        #self.data._pad_boundary(PAD_DX, PAD_DY) # TODO: Pad data once and save
+        self.data._pad_boundary(PAD_DX, PAD_DY) # TODO: Pad data once and save
     
     def match_template(self):
         return scarplet.calculate_best_fit_parameters(self.data, Scarp, self.d, self.age)
@@ -72,10 +73,9 @@ class Matcher(object):
 
     def save_template_match(self):
         self.results = self.match_template()
-        #self.clip_results(PAD_DX, PAD_DY) # Assume data is padded!
-        #i, j = self.get_clip_boundaries(PAD_DX, PAD_DY) # Assume data is padded!
-        #np.save(self.path + self.filename, self.results[:, j:-j, i:-i])
-        np.save(self.path + self.filename, self.results)
+        # XXX: Assume data is padded!
+        np.save(self.path + self.filename, self.results[:, PAD_DY:-PAD_DY, PAD_DX:-PAD_DX])
+        #np.save(self.path + self.filename, self.results)
         del self.results
 
     def set_params(self, age, d):
@@ -97,68 +97,79 @@ class Reducer(object):
         self.best_results = None
         
     def compare(self, file1, file2):
-        data1 = np.load(self.path + file1)
-        data2 = np.load(self.path + file2)
+        data1 = np.load(file1)
+        data2 = np.load(file2)
         mask = data1[-1,:,:] > data2[-1,:,:]
         data2[:,mask] = data1[:,mask]
         
-        os.remove(self.path + file1)
-        os.remove(self.path + file2)
+        os.remove(file1)
+        os.remove(file2)
         
         return data2
     
-    def mask_results(self):
-        mask = np.load('/efs/masks/' + self.tile_name + '_mask.npy')
-        self.best_results[:, mask] = np.nan
+    def mask_results(self, tile_name, results):
+        mask = np.load('/efs/masks/' + tile_name + '_mask.npy')
+        results[:, mask] = np.nan
+        return results
 
     def reduce(self, folder):
+        curdir = os.getcwd()
         self.path = self.path + '/' + folder
         print("Reducing results in {}...".format(self.path))
         results = os.listdir(self.path)
+        os.chdir(self.path)
         
         files_processed = 0
         while files_processed < self.num_files - 1: 
             if len(results) > 1:
-                start = timer()
                 sleep(1) # XXX: this is to avoid reading in a npy array as it is being written to disk
                 results1 = results.pop()
                 results2 = results.pop()
                 best = self.compare(results1, results2)
                 filename = uuid.uuid4().hex + '.npy'
-                np.save(self.path + filename, best)
+                np.save(filename, best)
                 files_processed += 1
-                stop = timer()
-                #print("Compared {} and {}".format(results1, results2))
-                #print("Elapsed time:\t {:.2f} s".format(stop-start))
-            results = os.listdir(self.path)
+            results = os.listdir('.')
 
+        os.chdir(curdir)
         self.best_results = self.path + os.listdir(self.path)[0]
 
     def reduce_all_results(self):
         curdir = os.getcwd()
         subgrids = os.listdir(self.path)
         os.chdir(self.path)
+        print("in: {}".format(os.getcwd()))
+
+        start = timer()
         
         files_processed = 0
         num_subgrids = len(subgrids)
         total_files = num_subgrids*(self.num_files - 1)
         while files_processed < total_files:
             for directory in subgrids:
-                os.chdir(directory)
                 results = os.listdir(directory)
+                os.chdir(directory)
                 if len(results) > 1:
                     results1 = results.pop()
                     results2 = results.pop()
                     best = self.compare(results1, results2)
                     filename = uuid.uuid4().hex + '.npy'
-                    np.save(self.path + directory + '/' + filename, best)
+                    np.save(filename, best)
                     files_processed += 1
                 os.chdir('..')
-
-        for tile in subgrids:
-            best_file = os.listdir(tile)[0]
-            save_file_to_s3(tile + '/' + best_file, tile + '_results.npy', bucket_name='scarp-tesing')
         os.chdir(curdir)
+                
+        stop = timer()
+        print("Elapsed time:\t {:.2f} s".format(stop - start))
+
+    def save_results(self):
+        subgrids = os.listdir(self.path)
+        for tile in subgrids:
+            best_file = self.path + '/' + tile + '/' + os.listdir(tile)[0]
+            results = np.load(best_file)    
+            results = mask_results(tile, results)
+            np.save(best_file, results)    
+            save_file_to_s3(best_file, tile + '_results.npy', bucket_name='scarp-testing')
 
     def save_best_results(self):
         save_file_to_s3(self.best_results, self.tile_name + '_results.npy', bucket_name='scarp-testing')
