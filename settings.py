4a97f548b4b8236327cbaa33c263e87817fc03d8
rmsare <robertmsare@gmail.com>
2017-09-21 14:00:40 -0700

    Update worker AMI again

--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@ AWS_BUCKET_NAME = 'scarp-data'
 AWS_INSTANCE_TYPE = 'c4.xlarge'
 AWS_KEY_NAME = 'aws-scarp'
 AWS_SECURITY_GROUP = 'sg-2b925f50'
-AWS_WORKER_AMI = 'ami-57c8342f'
+AWS_WORKER_AMI = 'ami-c2c438ba'
 
 SSH_LOCAL_KEY = '/home/rmsare/aws_keys/aws-scarp.pem'
 
