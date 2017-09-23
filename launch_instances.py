7f3c129cc017213fda9e80e2f74c9d978a66a830
rmsare <robertmsare@gmail.com>
2017-09-23 16:43:19 -0700

    Start many instances, ignore SSH certificate check to make spin up faster

--- launch_instances.py
+++ launch_instances.py
@@ -11,28 +11,27 @@ from settings import AWS_WORKER_AMI, AWS_BUCKET_NAME, AWS_INSTANCE_TYPE, AWS_KEY
 
 MAX_RETRIES = 100
 
-def launch_worker():
+def launch_workers(num_workers):
     
     connection = boto.ec2.connect_to_region('us-west-2')
     new_reservation = connection.run_instances(
                                     AWS_WORKER_AMI,
                                     key_name=AWS_KEY_NAME,
                                     instance_type=AWS_INSTANCE_TYPE,
-                                    security_group_ids=[AWS_SECURITY_GROUP])
-    instance = new_reservation.instances[0]
-    connection.create_tags([instance.id], 
-                            {"Name" : "Worker"})
+                                    security_group_ids=[AWS_SECURITY_GROUP],
+                                    min_count=num_workers,
+                                    max_count=num_workers)
 
-    #while instance.state == u'pending':
-    #    time.sleep(5)
-    #    instance.update()
+    for instance in new_reservation.instances:
+        connection.create_tags([instance.id], 
+                                {"Name" : "Worker"})
 
     #print("Launched worker: d = {:.2f}, age = {:.2f}".format(param[0], param[1]))
     #print("Public DNS: {}".format(dns))
     #print("State: {}".format(instance.state))
     #print("Launched worker: {}".format(instance.public_dns_name))
 
-    return instance
+    return new_reservation.instances
 
 def launch_reducer():
     
@@ -57,39 +56,34 @@ def upload_and_run_script(script, instance):
     instance.update()
     dns = instance.public_dns_name
 
-    for attempt in range(MAX_RETRIES):
-        try:
-            ssh_client = sshclient_from_instance(instance, SSH_LOCAL_KEY, user_name='ubuntu')
-        except:
-            #print("NoValidConnectionError: {}".format(e))
-            time.sleep(1)
-        else:
-            break
-    else:
-        raise RuntimeError("Reached maximum number of SSH connection attempts")
-
     with open('setup_and_run.sh', 'w') as f:
         f.write(script)
 
-    ssh_client.put_file('setup_and_run.sh', '/home/ubuntu/setup_and_run.sh')
-    os.remove('setup_and_run.sh')
-
     commands = []
-    commands.append(['ssh', '-o IdentityFile=/home/rmsare/aws_keys/aws-scarp.pem', 'ubuntu@' + dns, 'sudo /etc/init.d/screen-cleanup start'])
-    commands.append(['ssh', '-o IdentityFile=/home/rmsare/aws_keys/aws-scarp.pem', 'ubuntu@' + dns, 'screen -d -m chmod +x setup_and_run.sh'])
-    commands.append(['ssh', '-o IdentityFile=/home/rmsare/aws_keys/aws-scarp.pem', 'ubuntu@' + dns, 'screen -d -m ./setup_and_run.sh'])
+    commands.append(['scp', '-o IdentityFile=/home/rmsare/aws_keys/aws-scarp.pem', '-o StrictHostKeyChecking=no', 'setup_and_run.sh', 'ubuntu@' + dns + ':/home/ubuntu/'])
+    commands.append(['ssh', '-o IdentityFile=/home/rmsare/aws_keys/aws-scarp.pem', '-o StrictHostKeyChecking=no','ubuntu@' + dns, 'sudo /etc/init.d/screen-cleanup start'])
+    commands.append(['ssh', '-o IdentityFile=/home/rmsare/aws_keys/aws-scarp.pem', '-o StrictHostKeyChecking=no','ubuntu@' + dns, 'screen -d -m chmod +x setup_and_run.sh'])
+    commands.append(['ssh', '-o IdentityFile=/home/rmsare/aws_keys/aws-scarp.pem', '-o StrictHostKeyChecking=no','ubuntu@' + dns, 'screen -d -m ./setup_and_run.sh'])
     
     DEVNULL = open(os.devnull, 'w')
     for command in commands:
+        start = timer()
         subprocess.call(command, stdout=DEVNULL, stderr=subprocess.STDOUT)
+        stop = timer()
+        print('Execute command: {:.2f} s'.format(stop - start))
     DEVNULL.close()
 
 def get_worker_instances():
 
     connection = boto.ec2.connect_to_region('us-west-2')
-    reservations = connection.get_all_instances(filters={'tag:Name' : 'Worker'})
-    workers = [r.instances[0] for r in reservations if r.instances[0].state == u'running']
-    return workers
+    reservations = connection.get_all_instances(filters={'tag:Name' : 'Worker', 'instance-state-name' : 'running'})
+    return reservations[0].instances
+
+def run_job(instance, param):
+    script = STARTUP_SCRIPT.format(param[0], param[1])
+    upload_and_run_script(script, instance)
+    instance.update()
+    print("START: {} Started processing {:d} {:.2f}".format(instance.public_dns_name, param[0], param[1]))
 
 def relaunch_jobs():
 
@@ -102,12 +96,11 @@ def relaunch_jobs():
 
     workers = get_worker_instances()
     
+    start = timer()
     for age, instance in zip(ages, workers):
-        param = [d, age]
-        script = STARTUP_SCRIPT.format(param[0], param[1])
-        upload_and_run_script(script, instance)
-        instance.update()
-        print("RESTART: {} Started processing {:d} {:.2f}".format(instance.public_dns_name, d, age))
+        run_job(instance, [d, age])
+    stop = timer()
+    print("Re-launched jobs: {:.2f} s".format(stop - start))
 
 if __name__ == "__main__":
 
@@ -115,22 +108,20 @@ if __name__ == "__main__":
     min_age = 0
     max_age = 3.5 
     d_age = 0.1
-    num_ages = int((max_age - min_age) / d_age)
+    #num_ages = int((max_age - min_age) / d_age)
+    num_ages = int(sys.argv[1])
     ages = np.linspace(min_age, max_age, num_ages) 
 
+    start = timer()
     print("Launching {} nodes...".format(num_ages))
-    workers = [launch_worker() for _ in range(num_ages)]
-    
+    workers = launch_workers(num_ages)
+    stop = timer()
+    print("Spin up: {:.2f} s".format((stop - start) / num_ages)) 
+
     start = timer()
     for age, instance in zip(ages, workers):
-        param = [d, age]
-        script = STARTUP_SCRIPT.format(param[0], param[1])
-        upload_and_run_script(script, instance)
-        instance.update()
-        print("START: {} Started processing {:d} {:.2f}".format(instance.public_dns_name, d, age))
+        run_job(instance, [d, age])
     stop = timer()
-
-    print("Spin up:\t\t {:.2f} s".format(stop - start))
 
     #reducer_instance = launch_reducer()
     #reducer_script = REDUCER_SCRIPT.format(num_ages)
