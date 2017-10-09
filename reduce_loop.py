4002683d2cc81a60908244f3db050f3ee2646ab5
rmsare <robertmsare@gmail.com>
2017-10-09 11:01:12 -0700

    Update reduce_loop arguments

--- reduce_loop.py
+++ reduce_loop.py
@@ -31,19 +31,24 @@ def reduce_current_data(num_files=35):
 #    pass
 
 if __name__ == "__main__":
+    d = int(sys.argv[1])
+    num_workers = int(sys.argv[2])
+    remote_dir = sys.argv[3]
+    batch_size = int(sys.argv[4])
+
     logging.config.fileConfig('logging.conf')
     logger = logging.getLogger('scarp_reduce')
 
     last_key = ''
     while last_key:
         logger.info("Downloading batch beginning with " + last_key)
-        download_data(remote_dir, marker=last_key)
+        download_data(remote_dir, marker=last_key, batch_size=batch_size)
         logger.info("Launching matching jobs")
-        launch_jobs()
+        launch_jobs(d, num_workers)
         while len(os.listdir('/efs/results')) != 0:
             sleep(5)
         logger.info("Reducing current results")
-        reduce_current_data()
+        reduce_current_data(num_workers)
         logger.info("Deleting data and working files")
         delete_local_files()
         logger.info("Uploading log")
