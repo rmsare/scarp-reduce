55c5899e6f7792613c5ad23942cb3c32cf16cf68
Rob Sare <robertmsare@gmail.com>
2017-10-09 23:06:44 +0000

    Update AMI, change key location

--- settings.py
+++ settings.py
@@ -3,9 +3,9 @@ AWS_WORKER_INSTANCE_TYPE = 'c4.xlarge'
 AWS_REDUCER_INSTANCE_TYPE = 't2.medium'
 AWS_KEY_NAME = 'aws-scarp'
 AWS_SECURITY_GROUP = 'sg-2b925f50'
-AWS_WORKER_AMI = 'ami-28ad6a50'
+AWS_WORKER_AMI = 'ami-9ac601e2'
 
-SSH_LOCAL_KEY = '/home/rmsare/aws_keys/aws-scarp.pem'
+SSH_LOCAL_KEY = '/home/ubuntu/aws-scarp.pem'
 
 REDUCER_SCRIPT = """#!/bin/bash
 sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 fs-014be5a8.efs.us-west-2.amazonaws.com:/ /efs 
