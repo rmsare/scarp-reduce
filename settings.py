889c1c8da30ff4e520ba553c8de3565b976626c2
Rob Sare <robertmsare@gmail.com>
2017-10-10 04:23:07 +0000

    Update AMI...

--- settings.py
+++ settings.py
@@ -3,25 +3,21 @@ AWS_WORKER_INSTANCE_TYPE = 'c4.xlarge'
 AWS_REDUCER_INSTANCE_TYPE = 't2.medium'
 AWS_KEY_NAME = 'aws-scarp'
 AWS_SECURITY_GROUP = 'sg-2b925f50'
-AWS_WORKER_AMI = 'ami-24cf085c'
+AWS_WORKER_AMI = 'ami-34e0274c'
 
 SSH_LOCAL_KEY = '/home/ubuntu/aws-scarp.pem'
 
 REDUCER_SCRIPT = """#!/bin/bash
 sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 fs-014be5a8.efs.us-west-2.amazonaws.com:/ /efs 
 sudo chown -R ubuntu /efs
-#cd /home/ubuntu/scarplet-python
-#git pull origin master
 cd /home/ubuntu/scarp-reduce
 #git pull origin master
-ipython reduce.py {}
+ipython reduce_loop.py {} {} {} {}
 #sudo shutdown -h now"""
 
 WORKER_SCRIPT = """#!/bin/bash
 sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 fs-014be5a8.efs.us-west-2.amazonaws.com:/ /efs 
 sudo chown -R ubuntu /efs
-#cd /home/ubuntu/scarplet-python
-#git pull origin master
 cd /home/ubuntu/scarp-reduce
 #git stash
 #git pull origin master
