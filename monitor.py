5506a5455d7e1a8f585637c818adc1121aaf0801
Rob Sare <robertmsare@gmail.com>
2017-11-07 17:25:25 +0000

    Measure CPU usage by running average

--- monitor.py
+++ monitor.py
@@ -17,25 +17,32 @@ if __name__ == "__main__":
     this_pid = os.getpid()
     os.chdir('/home/ubuntu/')
     sleep(interval) # Allow matching job some startup time
+    periods = 1.
+    total_cpu_usage = 0.
+    average_cpu_usage = 0.
 
     while True:
-        if psutil.cpu_percent() < 25.0:
+        if average_cpu_usage < 50.0:
             commands = []
-            commands.append(['sudo', 'umount', '-f', '/efs'])
-            commands.append(['sudo', 'mount', '-t', 'nfs4', '-o', 'nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2', 'fs-2fab1c86.efs.us-west-2.amazonaws.com:/', '/efs'])
-            commands.append(['sudo', 'chown', '-R', 'ubuntu', '/efs'])
+            #commands.append(['sudo', 'umount', '-f', '/efs'])
+            #commands.append(['sudo', 'mount', '-t', 'nfs4', '-o', 'nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2', 'fs-2fab1c86.efs.us-west-2.amazonaws.com:/', '/efs'])
+            #commands.append(['sudo', 'chown', '-R', 'ubuntu', '/efs'])
 
             for p in psutil.process_iter():
-
                 if 'ipython' in p.name() and p.pid != this_pid:
                     commands.append(['sudo', 'kill', '{}'.format(p.pid)])
 
             commands.append(['sudo', 'sysctl', '-w',  'vm.drop_caches=3'])
-            commands.append(['screen', '-d',  '-m', './runme.sh'])
+            commands.append(['screen', '-wipe'])
+            commands.append(['./runme.sh'])
 
             for c in commands:
                 subprocess.call(c)
-        else:
-            sleep(interval)
+        
+        sleep(interval)
+            
+        periods += 1
+        total_cpu_usage += psutil.cpu_percent()
+        average_cpu_usage = total_cpu_usage / periods
 
         
