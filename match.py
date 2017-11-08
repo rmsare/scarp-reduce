ec04329b4120805cde12ce13526878349a6e66e0
Rob Sare <robertmsare@gmail.com>
2017-11-08 18:53:21 +0000

    Add shuffling for job start to see if this reduces competition when orphaned jobs collide in nearly-full dirs

--- match.py
+++ match.py
@@ -26,6 +26,7 @@ if __name__ == "__main__":
     logger.info("Starting Matcher for {} {:.2f}".format(d, ages[0]))
 
     files = os.listdir(local_data_directory)
+    np.random.shuffle(files)
     finished_processing = False
     while not finished_processing:
         for tile in files:
