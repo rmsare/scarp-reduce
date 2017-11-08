acab30afb29072a10dbf6339851c7af5a85f84d2
Rob Sare <robertmsare@gmail.com>
2017-11-08 18:33:52 +0000

    Remove directory shuffling

--- match.py
+++ match.py
@@ -26,7 +26,6 @@ if __name__ == "__main__":
     logger.info("Starting Matcher for {} {:.2f}".format(d, ages[0]))
 
     files = os.listdir(local_data_directory)
-    np.random.shuffle(files)
     finished_processing = False
     while not finished_processing:
         for tile in files:
@@ -35,5 +34,4 @@ if __name__ == "__main__":
             worker.process(d, ages)
             logger.debug("Finished processing {}, {} {:.2f}".format(tile, d, ages[0]))
         files = os.listdir(local_data_directory)
-        np.random.shuffle(files)
         finished_processing = len(files) == 0
