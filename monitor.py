f9d3a21d8ee7b643b040ee396675771c8a04bb48
Rob Sare <robertmsare@gmail.com>
2017-11-09 04:17:50 +0000

    Decrease CPU usage threshold

--- monitor.py
+++ monitor.py
@@ -13,7 +13,7 @@ if __name__ == "__main__":
     logger = logging.getLogger('scarp_reduce')
 
     interval = 15 
-    max_periods = 8
+    max_periods = 4
     
     this_pid = os.getpid()
     os.chdir('/home/ubuntu/')
@@ -34,7 +34,7 @@ if __name__ == "__main__":
             total_cpu_usage += psutil.cpu_percent()
             average_cpu_usage = total_cpu_usage / periods
 
-        if average_cpu_usage < 25.0:
+        if average_cpu_usage < 10.0:
             commands = []
             #commands.append(['sudo', 'umount', '-f', '/efs'])
             #commands.append(['sudo', 'mount', '-t', 'nfs4', '-o', 'nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2', 'fs-2fab1c86.efs.us-west-2.amazonaws.com:/', '/efs'])
