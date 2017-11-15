f0dbfea6f5c001941abdae38a339e25a427c2c35
rmsare <robertmsare@gmail.com>
2017-11-14 19:07:58 -0800

    Remove nodata fill

--- tile_data.py
+++ tile_data.py
@@ -3,18 +3,21 @@ import rasterio
 
 import numpy as np
 
+from rasterio.fill import fillnodata
+
 def define_tile_transform(transform, row_idx, col_idx):
     dx = transform[1]
     dy = np.abs(transform[5])
     
     ulx = transform[0] + col_idx * dx
-    uly = transform[3] + row_idx * dy
+    uly = transform[3] - row_idx * dy
 
     return (ulx, dx, 0, uly, 0, -1*dy)
 
-def tile_data(filename, nx, ny, code='tiles/fg'):
+def tile_data(filename, width_m, height_m, code='tiles/fg'):
     with rasterio.open(filename, 'r') as dataset:
-        data = dataset.read(1)
+        data = dataset.read(1).astype(rasterio.float32)
+        data[data == 0] = np.nan
         num_rows, num_cols = data.shape
         
         transform = dataset.transform
@@ -23,10 +26,13 @@ def tile_data(filename, nx, ny, code='tiles/fg'):
         dx = transform[1]
         dy = np.abs(transform[5])
 
+        nx = int(width_m / dx)
+        ny = int(height_m / dy)
+
         for i in range(0, num_rows, ny):
             for j in range(0, num_cols, nx):
-                llx = ulx + j * dx
-                lly = uly + i * dy
+                llx = ulx + j * dx 
+                lly = uly - i * dy - height_m
                 out_filename = code + str(int(llx / 1000)) + '_' + str(int(lly / 1000)) + '.tif'
                 new_transform = define_tile_transform(transform, i, j) 
                 
@@ -44,11 +50,11 @@ def tile_data(filename, nx, ny, code='tiles/fg'):
 
 if __name__ == "__main__":
     filename = sys.argv[1]
-    nx = int(sys.argv[2])
-    ny = int(sys.argv[3])
+    width = int(sys.argv[2])
+    height = int(sys.argv[3])
 
     if not os.path.exists('tiles'):
         os.mkdir('tiles')
 
-    tile_data(filename, nx, ny)
+    tile_data(filename, width, height)
 
