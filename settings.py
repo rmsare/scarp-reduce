052de4fc937e7932371c8cc474755ac363aadba6
Robert Sare <rmsare@stanford.edu>
2018-01-08 09:00:54 -0800

    Delete settings

--- settings.py
+++ settings.py
@@ -2,8 +2,8 @@ AWS_BUCKET_NAME = 'scarp-data'
 AWS_WORKER_INSTANCE_TYPE = 'c4.2xlarge'
 AWS_REDUCER_INSTANCE_TYPE = 't2.medium'
 AWS_KEY_NAME = 'aws-scarp'
-AWS_SECURITY_GROUP = 'sg-2b925f50'
-AWS_WORKER_AMI = 'ami-d8f136a0'
+AWS_SECURITY_GROUP = 'xxx'
+AWS_WORKER_AMI = 'xxx'
 
 SSH_LOCAL_KEY = '/home/ubuntu/aws-scarp.pem'
 
@@ -20,7 +20,7 @@ ipython -c "%run match.py {} {} {} {} {} {}"
 #sudo shutdown -h now"""
 
 STARTUP_SCRIPT = """#!/bin/bash
-sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 fs-2fab1c86.efs.us-west-2.amazonaws.com:/ /efs 
+sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 xxx.efs.us-west-2.amazonaws.com:/ /efs 
 sudo chown -R ubuntu /efs
 mkdir /efs/data /efs/results /efs/logs
 cd /home/ubuntu/scarp-reduce
