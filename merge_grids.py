7ad7eeb790537be915968e0bc890a5ad0e994506
rmsare <robertmsare@gmail.com>
2018-01-17 15:33:09 -0800

    Fix padding indices -- bug from old version?

--- merge_grids.py
+++ merge_grids.py
@@ -1,10 +1,14 @@
 import os, sys
 import subprocess
 import numpy as np
-from osgeo import gdal
+from osgeo import osr, gdal
 from itertools import product
 
-sys.path.append('/home/ubuntu/scarplet-python/scarplet')
+from rasterio.fill import fillnodata
+
+from progressbar import Bar, Percentage, ProgressBar, ETA
+
+sys.path.append('/media/rmsare/GALLIUMOS/src/scarplet-python/scarplet')
 import dem
 
 def neighbors(filename, offset=2):
@@ -35,28 +39,28 @@ def pad_with_neighboring_values(filename, pad, data_dir='/media/rmsare/GALLIUMOS
                 src_data = np.nan * np.ones((ny, nx))
 
             if i == 0: # SW corner
-                pad_values = src_data[0:pad, -pad:]
+                pad_values = src_data[-pad:, 0:pad]
                 dest_data[pad + ny:2*pad + ny, 0:pad] = pad_values 
             elif i == 1: # W edge 
-                pad_values = src_data[:, -pad:]
+                pad_values = src_data[:, 0:pad]
                 dest_data[pad:nx + pad, 0:pad] = pad_values 
             elif i == 2: # NW corner
-                pad_values = src_data[-pad:, -pad:]
+                pad_values = src_data[0:pad, 0:pad]
                 dest_data[0:pad, 0:pad] = pad_values 
             elif i == 3: # S edge 
-                pad_values = src_data[0:pad, :]
+                pad_values = src_data[-pad:, :]
                 dest_data[pad + ny:2*pad + ny, pad:pad + nx] = pad_values 
             elif i == 5: # N edge 
-                pad_values = src_data[-pad:, :]
+                pad_values = src_data[0:pad, :]
                 dest_data[0:pad, pad:pad + nx] = pad_values 
             elif i == 6: # SE corner
-                pad_values = src_data[0:pad, 0:pad]
+                pad_values = src_data[-pad:, -pad:]
                 dest_data[pad + ny:2*pad + ny, pad + nx:2*pad + nx] = pad_values 
             elif i == 7: # E edge 
-                pad_values = src_data[:, 0:pad]
+                pad_values = src_data[:, -pad:]
                 dest_data[pad:pad + ny, pad + nx:2*pad + nx] = pad_values 
             elif i == 8: # NE corner
-                pad_values = src_data[-pad:, 0:pad]
+                pad_values = src_data[0:pad, -pad:]
                 dest_data[0:pad, pad + ny: 2*pad + ny] = pad_values 
 
     return dest_data
@@ -77,30 +81,48 @@ def sort_by_utm_northing(filenames):
     return list(filenames[idx])
 
 if __name__ == "__main__":
-    source_dir = '/media/rmsare/GALLIUMOS/data/ot_data/tif/1m/'
-    dest_dir = '/media/rmsare/GALLIUMOS/data/ot_data/merged_2km/'
+    
+    pad = 1500
+    proc_dir = '/media/rmsare/GALLIUMOS/data/ot_data/olema/'
+    source_dir = '/media/rmsare/GALLIUMOS/data/ot_data/merged_3km_2m_full/'
+    dest_dir = '/media/rmsare/GALLIUMOS/data/ot_data/nsaf_6km/'
 
-    files = os.listdir(source_dir)
+    files = os.listdir(proc_dir)
     files = sort_by_utm_northing(files)
+    pbar = ProgressBar(widgets=[Percentage(), ' ', Bar(), ' ', ETA()], maxval=len(files))
+    pbar.start()
 
-    for f in files:
-        grids = neighbors(f)
-        grids = set.intersection(set(grids), set(files))
-        if len(grids) > 1:
-            file_args = [source_dir + nb for nb in grids]
-            command = ['gdal_merge.py', '-o', dest_dir + f, '-of', 'GTiff', '-init', 'nan']
-            command.extend(file_args)
+    for i, f in enumerate(files):
+        padded_data = pad_with_neighboring_values(f, pad, data_dir=source_dir)
+        nodata_mask = ~np.isnan(padded_data)
+        num_nodata = np.sum(~nodata_mask)
+        while num_nodata > 0:
+            padded_data = fillnodata(padded_data, mask=nodata_mask)
+            nodata_mask = ~np.isnan(padded_data)
+            num_nodata = np.sum(~nodata_mask)
+        nrows, ncols = padded_data.shape
 
-            print("Merging {}".format(f))
-            subprocess.call(command)
+        data_file = source_dir + f
+        inraster = gdal.Open(data_file)
+        transform = inraster.GetGeoTransform()
+        
+        driver = gdal.GetDriverByName('GTiff')
+        outraster = driver.Create(dest_dir + f, ncols, nrows, 1, gdal.GDT_Float32)
+        outraster.SetGeoTransform(transform)
 
-            #for x in grids:
-            #   files.remove(x)
+        out_band = outraster.GetRasterBand(1)
+        out_band.WriteArray(padded_data)
+        out_band.FlushCache()
+        
+        srs = osr.SpatialReference()
+        srs.ImportFromWkt(inraster.GetProjectionRef())
+        outraster.SetProjection(srs.ExportToWkt())
+        pbar.update(i+1)
 
     print("Deleting swath edge tiles...")
     for f in os.listdir(dest_dir):
         raster = gdal.Open(dest_dir + f)
         not_square = raster.RasterXSize != raster.RasterYSize
-        too_small = raster.RasterXSize < 2000 or raster.RasterYSize < 2000
+        too_small = raster.RasterXSize < 1500 or raster.RasterYSize < 1500
         if not_square or too_small:
             os.remove(dest_dir + f)
